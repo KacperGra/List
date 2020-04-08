20e4e775991a718457f291ef4a72e9b35969d32c
KacperGra <kacperktbm@gmail.com>
2020-04-08 13:34:25 +0200

    Basic fixes.

--- Include/List.h
+++ Include/List.h
@@ -1,8 +1,78 @@
-//
-// Created by kacper on 08.04.2020.
-//
-
 #ifndef LIST_LIST_H
 #define LIST_LIST_H
 
 #endif //LIST_LIST_H
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+
+template<class T>
+class Element
+{
+public:
+    T value;
+    std::shared_ptr<Element<T>> next;
+
+    Element(T _Nvalue)
+    {
+        value = _Nvalue;
+        next = nullptr;
+    }
+};
+
+template<class T>
+class List
+{
+    std::shared_ptr<Element<T>> first;
+public:
+
+    List()
+    {
+        first = nullptr;
+    }
+
+    void push_back(T x)
+    {
+        auto NewElement = std::make_shared<Element<T>>(Element<T>(_Nvalue)
+
+        if (first == nullptr)
+        {
+            first = newElement;
+            return;
+        }
+
+        std::shared_ptr<Element<T>> current = first;
+
+        while (current->next != nullptr)
+        {
+            current = current->next;
+        }
+
+        current->next = newElement;
+    }
+
+    void print()
+    {
+        std::shared_ptr<Element<T>> current = first;
+        while (current != nullptr)
+        {
+            std::cout << current->value << std::endl;
+            current = current->next;
+        }
+    }
+
+    unsigned int size()
+    {
+        unsigned int counter = 0;
+
+        std::shared_ptr<Element<T>> current = first;
+        while (current != nullptr)
+        {
+            ++counter;
+            current = current->next;
+        }
+        return counter;
+    }
+};
--- Test/listTest.cpp
+++ Test/listTest.cpp
@@ -1,12 +1,15 @@
-#include "./main.cpp"
+#include "List.h"
 #include <gtest/gtest.h>
 
 using namespace ::testing;
 
 
-TEST(ListTest, FloatListSizeShouldEqual1)
+TEST(ListTest, IntSizeShouldBe4)
 {
-    List<float> floatList;
-    floatList.push_back(2.5);
+    List<int> floatList;
+    floatList.push_back(2);
+    floatList.push_back(5);
+    floatList.push_back(1);
+    floatList.push_back(234);
     ASSERT_EQ(1, floatList.size());
 }
